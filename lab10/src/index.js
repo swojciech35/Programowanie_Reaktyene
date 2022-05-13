cfcf959762e676e3403d25b3db67fb4cb296d423
Schabiik <swojciech12@gmail.com>
2022-05-13 11:57:55 +0200

    lab 10

---
+++ lab10/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+//pamiętać proszę o imporcie komponentu Posts oraz Home
+import Home from './components/home';
+import Posts from './components/posts';
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}>
+
+                    <Route path="home" element={<Home/>}/>
+
+                    <Route path="posts" element={<Posts/>}/>
+
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+reportWebVitals();
